4bf6caf72b0a0acfef3a5cead12249ca5fd8f49b
Waldir Júnior <waldirjunior.gpn@gmail.com>
2025-03-02 12:18:25 -0300

    Primeiro Commit

---
+++ desafio-unibanco-99/src/main/java/com/desafio99/desafio_unibanco_99/DesafioUnibanco99Application.java
@@ -0,0 +1,13 @@
+package com.desafio99.desafio_unibanco_99;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesafioUnibanco99Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesafioUnibanco99Application.class, args);
+	}
+
+}
---
+++ desafio-unibanco-99/src/test/java/com/desafio99/desafio_unibanco_99/DesafioUnibanco99ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.desafio99.desafio_unibanco_99;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesafioUnibanco99ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
